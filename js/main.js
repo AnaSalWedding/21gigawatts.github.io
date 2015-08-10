c2dac5b7eded6fcdf6a7b23505978439aec21048
Salvatore <dested@gmail.com>
2015-08-09 22:24:33 -0700

    updated page content

--- js/main.js
+++ js/main.js
@@ -39,9 +39,8 @@ $(window).load(function(){
 
 //==== Run on Document Ready ========
 $(document).ready(function(){
-
 //=====>  Countdown (Edit this with your own date)  <====
-$("#countdown").countdown("2015/11/22", function(event) {
+$("#countdown").countdown(new Date(2015,9,21,16,29), function(event) {
   var $this = $(this).html(event.strftime(''
      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
